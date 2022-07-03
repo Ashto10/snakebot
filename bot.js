652d94922ffe41d82a6acb6964b99b48fa3c8bce
Juliette Augustine <contact@julietteaugustine.com>
2022-07-03 01:18:16 -0400

    Disable MongoDB for now

--- bot.js
+++ bot.js
@@ -3,9 +3,10 @@ const fs = require('fs');
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-const mongoose = require('mongoose');
-const url = `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@ds153824.mlab.com:53824/snakebot`;
-mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
+// Need to move database to new host at some point
+// const mongoose = require('mongoose');
+// const url = `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@ds153824.mlab.com:53824/snakebot`;
+// mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const Snakebot = require('./app/models/snakebot');
 const snakebot = new Snakebot();
@@ -17,8 +18,8 @@ const conversation = require('./app/conversation/conversation');
 client.timers = [];
 
 client.on('ready', () => {
-  console.log("*Hacker voice* I'm in.");
-  commandStartup(client);
+    console.log("*Hacker voice* I'm in.");
+    commandStartup(client);
 })
 
 client.on('message', message => {
@@ -30,10 +31,10 @@ client.on('message', message => {
     // Run commands
     if (message.content.startsWith('$')) {
         return commandDirectory(client, message);
-    } 
+    }
 
     // If someone mentions Snakebot, have it respond
-    if(message.content.search(/\s?snakebot\s?/i) !== -1) {
+    if (message.content.search(/\s?snakebot\s?/i) !== -1) {
         return conversation(client, snakebot, message);
     }
 })
